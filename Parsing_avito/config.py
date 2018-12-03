6c6b0a39beb6b9c33d03d65b04108f3388402541
Илья Симонов <issim91@outlook.com>
2018-12-03 23:34:21 +0400

    add extra functions

--- Parsing_avito/config.py
+++ Parsing_avito/config.py
@@ -1,31 +1,51 @@
 import requests
 from random import choice, uniform
 from bs4 import BeautifulSoup
+import csv
 
 def get_html(url, useragent=None, proxy=None):
-    s = requests.Session()
-    r_cookie = s.get(url, headers=useragent, proxies=proxy)
-    cookie = r_cookie.cookies
-    print(cookie)
-    print('---------------------------')
-    # cookies = dict(cookies_are='working')
-    # r = requests.get(url, cookies=cookies)
-    r = s.get(url, headers=useragent, proxies=proxy, cookies=cookie)
-    print(r.cookies)
-    # print(r.text)
-    # 'Set-Cookie': 'sessid=27d75dcaac2e65605ade1ab914910f7a.1543591643
-    # u=2b7k8fgj.omudut.fxqi8ns4ro;
-    # v=1543591643
-    # dfp_group=8
-    # print(r.cookies)
+    timeout = uniform(12, 25)
+    r = requests.get(url, headers=useragent, proxies=proxy, timeout=timeout)
     return r.text
 
 def get_proxy():
     useragents = open('Parsing_avito/useragent.txt').read().split('\n')
-    proxies = open('Parsing_avito/proxy-ip.txt').read().split('\n')
-    proxy = {'http': 'http://' + choice(proxies)}
     useragent = {'User-Agent': choice(useragents)}
+
+    proxies = open('Parsing_avito/proxy-ip.txt', 'r')
+    ip = str(proxies.readline()).replace("\n", "")
+    proxy = {'http': 'http://' + ip}
+    lines = proxies.readlines()
+    proxies.close()
+
+    proxies = open('Parsing_avito/proxy-ip.txt', 'w')
+    for line in lines:
+      if line != ip:
+        proxies.write(line)
+    proxies.write('\n')
+    proxies.write(ip)
+    proxies.close()
     return proxy, useragent
+
+
+def file_all_cat():
+    urls = open('Parsing_avito/cat.csv').read().split('\n')
+    list_url = []
+    list_name = []
+    for url in urls:
+            list_url.append(url.split(';')[-1])
+            list_name.append(url.split(';')[0])
+    return list_url, list_name
+
+def file_all_sity():
+    urls = open('Parsing_avito/all_sity.csv').read().split('\n')
+    eng = []
+    rus = []
+    for url in urls:
+            eng.append(url.split(';')[-1])
+            rus.append(url.split(';')[0])
+    return eng, rus
+    
 
 def get_phone(url):
     proxy, useragent = get_proxy()
@@ -33,3 +53,14 @@ def get_phone(url):
     soup = BeautifulSoup(html, "lxml")
     phone = soup.find('div', class_='_1DzgK').find('a').get('href').split(':')[-1]
     return phone
+
+# Добавляем названия столбцов в файле
+def add_name_row():
+    with open('Parsing_avito/all_prod.csv', 'a', encoding='cp1251', newline='') as f:
+        writen = csv.writer(f, delimiter= ';')
+        writen.writerow( (  "Название категории",
+                            "Название товара",
+                            "Цена",
+                            "Описание",
+                            "Номер телефона",
+                            "Картинки") )
